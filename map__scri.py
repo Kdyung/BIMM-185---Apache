72e0a127ac0dc8452c54e4dc7271050852c5d5ac
Kdyung <kyung8888@gmail.com>
2014-05-14 10:15:10 -0700

    Delete map__scri.py

--- map__scri.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-#BIMM 185 EMap project
-#In the interest of implementing the pipeline, this non web-implemented prototype is made to see how to communicate between the programs.
-#Until I figure out how to make this communicate with the web server, this is how I am doing it.
-
-import 
